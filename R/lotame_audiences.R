5402e0b765b3175443d92333adc646eb97469e49
Jack Palmer <jpalmer@3qdigital.com>
2017-02-08 14:06:10 -0500

    Lotame update

--- R/lotame_audiences.R
+++ R/lotame_audiences.R
@@ -3,9 +3,13 @@
 #'This function returns a data frame of audiences for the requested client.
 #'  
 #'@export 
-lotame_audiences <- function(client_id){
+lotame_audiences <- function(client_id,
+                             page_count = 50){
   options(scipen=999)
-  path <- paste0("https://api.lotame.com/2/audiences?client_id=",client_id)
+  path <- paste0("https://api.lotame.com/2/audiences?client_id=",
+                 client_id,
+                 "&page_count=",
+                 page_count)
   service_ticket <- lotame_service_ticket(path)
   req <- jsonlite::fromJSON(paste0(path,"&ticket=",service_ticket))
   audiences <- req$Audience
