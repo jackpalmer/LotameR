d826208b9bfdc5c95bd3f03484d7391567e20701
Jack Palmer <jpalmer@3qdigital.com>
2017-02-08 14:37:14 -0500

    Add lotame_campaign call

---
+++ R/lotame_campaign.R
@@ -0,0 +1,16 @@
+#'@title Lotame Audiences
+#'@description
+#'This function returns a data frame of audiences for the requested client.
+#'  
+#'@export 
+lotame_campaign <- function(campaign_id,
+                            lookback_days = 0){
+  options(scipen=999)
+  path <- paste0("https://api.lotame.com/2/statistics/campaigns/",
+                 campaign_id,
+                 "&lookbook_days=",
+                 lookbook_days)
+  service_ticket <- lotame_service_ticket(path)
+  req <- jsonlite::fromJSON(paste0(path,"&ticket=",service_ticket))
+  return(req)
+}
